2f96b01cf319a9caafeefab6e63a5d3e4c6f839c
Mensun <mensun@bu.edu>
2025-02-15 02:02:16 -0500

    finished styling JokeCards

--- src/components/ui/JokeCards.tsx
+++ src/components/ui/JokeCards.tsx
@@ -1,14 +1,44 @@
 import { Joke } from "../../interfaces/Joke.ts";
+import styled from "styled-components";
 
 function JokeCards(props: { jokes: Joke[] }) {
+  /* css */
+  const StyledDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    background-color: lightblue;
+  `
+  const StyledJokeUl = styled.ul`
+    display: flex;
+    flex-direction: column;
+    width: 50vw;
+  `
+
+  const StyledJokeLi = styled.li`
+    list-style-type: none;
+    background-color: skyblue;
+    padding: 10px;
+    margin: 10px;
+    border: 2px blue solid;
+  `
+
+  const StyledSpan = styled.span`
+    text-align: center;
+    text-transform: uppercase;
+  `
   return (
-    <ul>
-      {props.jokes.map((joke: Joke) => (
-        <li key={joke.id}>
-          Joke id: {joke.id}
-        </li>
-      ))}
-    </ul>
+    <StyledDiv>
+      <StyledJokeUl>
+        {/* map each joke to a container */}
+        {props.jokes.map((joke: Joke) => (
+          <StyledJokeLi key={joke.id}>
+            <StyledSpan><h3>{joke.type} (#{joke.id})</h3></StyledSpan>
+            <h4>Q: {joke.setup}</h4>
+            <p>A: {joke.punchline}</p>
+          </StyledJokeLi>
+        ))}
+      </StyledJokeUl>
+    </StyledDiv>
   )
 }
 
